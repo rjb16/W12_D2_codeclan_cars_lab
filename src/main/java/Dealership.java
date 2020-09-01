45b73ad2652b3f88b35ba578b0d9fb66bd703007
Stephen <stephen_h369@live.co.uk>
2020-09-01 14:59:38 +0100

    dealership tests for getters and add stock pass

--- src/main/java/Dealership.java
+++ src/main/java/Dealership.java
@@ -1,2 +1,26 @@
+import car.Car;
+
+import java.util.ArrayList;
+
 public class Dealership {
+
+    private ArrayList<Car> stock;
+    private double till;
+
+    public Dealership(double till) {
+        this.stock = new ArrayList<Car>();
+        this.till = till;
+    }
+
+    public ArrayList<Car> getStock() {
+        return stock;
+    }
+
+    public double getTill() {
+        return till;
+    }
+
+    public void addStock(Car car) {
+        stock.add(car);
+    }
 }
---
+++ src/test/java/DealershipTest.java
@@ -0,0 +1,70 @@
+import car.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class DealershipTest {
+
+    Dealership dealership;
+    Car car1;
+    Car car2;
+    ArrayList<Car> cars;
+    CombustionEngine engine1;
+    Transmission transmission1;
+    Tyres tyres1;
+    ElectricMotor engine2;
+    Transmission transmission2;
+    Tyres tyres2;
+
+    @Before
+    public void before() {
+        dealership = new Dealership(123456.00);
+        engine1 = new CombustionEngine(1600, 4);
+        transmission1 = new Transmission("Manual", 6);
+        tyres1 = new Tyres("Michelin", "205/17/40");
+        engine2 = new ElectricMotor();
+        transmission2 = new Transmission("Manual", 6);
+        tyres2 = new Tyres("Michelin", "205/17/40");
+        car1 = car1 = new Car(
+                "Ford",
+                "Fiesta",
+                CarType.PETROL,
+                engine1,
+                transmission1,
+                tyres1,
+                12500.00,
+                "Black",
+                "Hatchback",
+                3);
+        car2 = new Car(
+                "Tesla",
+                "Model 3",
+                CarType.ELECTRIC,
+                engine2,
+                transmission2,
+                tyres2,
+                50000.00,
+                "Black",
+                "Saloon",
+                5);
+    }
+
+    @Test
+    public void canGetStock() {
+        assertEquals(0, dealership.getStock().size());
+    }
+
+    @Test
+    public void canGetTillAmount() {
+        assertEquals(123456.00, dealership.getTill(), 0.01);
+    }
+
+    @Test
+    public void canAddStock() {
+        dealership.addStock(car1);
+        assertEquals(1, dealership.getStock().size());
+    }
+}
